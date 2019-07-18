4280c44fc97e23faa8fcaae53718dc3b9ad792ec
Lou <antilou86@gmail.com>
2019-07-18 16:58:21 -0400

    WIP: loaded dependencies for styled-components and semantic UI. took forever on my machine

--- src/components/ImageCard/Description/Description.js
+++ src/components/ImageCard/Description/Description.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React from 'react'
+import { Container, Divider } from 'semantic-ui-react'
+import styled from 'styled-components'
 
-function Description(props) {
-    const {descriptionData} = props;
-    return (
-        <div>
-            <p>{descriptionData}</p>
-        </div>
-    )
-}
+const StyledDiv = styled.div`
+    width: 80%;
+    margin:0 auto;
+    background-color: #afb8c7;
+    padding-bottom: 20px;
+    color: #1a222e;
+`;
 
 
-export default Description;
+const Description = (props) => (
+  <StyledDiv>
+    <Container textAlign='left'>
+      <b>Description</b>
+      <Divider />
+      <p>{props.descriptionData}</p>
+    </Container>
+  </StyledDiv>
+)
+
+export default Description
--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,28 +1,43 @@
 import React from "react";
 import Iframe from 'react-iframe';
 import Description from "./Description/Description.js";
+import styled from 'styled-components'
+
 
 
 function ImageCard(props) {
 const {imageData, descriptionData} = props;
-console.log(props);
 
+let ImageWrapper = styled.div`
+    width: 80%;
+    height: auto;
+    background-color: #afb8c7;
+    margin: 0 auto;
+    overflow: hidden;
+`;
+
+const testing = {
+    fit:'contain', 
+    display: 'initial',
+    position: 'relative'  }
+;
 
 let checker = (value) => { 
     if (value.media_type === "video"){
     return (
+        
         <Iframe url={value.url}
-                width="800px"
-                height="500px"
-                display="initial"
-                position="relative"
+                style={testing}
                 allowFullScreen/>
+        
     )} else {
-    return (<img src={value.url} alt="a sweet pic straight from Nasa"/>)
+    return (<img src={value.url} alt="a sweet pic straight from Nasa" />)
 }}    
     return (
         <div>
+        <ImageWrapper>
             {imageData && checker(imageData)}
+        </ImageWrapper>
             <Description descriptionData={descriptionData}/>
         </div>
     )}
